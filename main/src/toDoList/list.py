80cc97427323b4bc8de279c6344d4e08b4f23bb2
Adam <Adam.ja.com@op.pl>
2023-04-01 12:32:31 +0200

    toDoList simply backend done

--- main/src/toDoList/list.py
+++ main/src/toDoList/list.py
@@ -1,25 +1,40 @@
+from .task import Task
+
+
 class ToDoList:
     def __init__(self):
-        self.tasks = []
+        self.tasks: list[Task] = []
 
-    def add(self, elem):
-        self.tasks.append(elem)
+    def add(self, name: str, date: str):
+        task = Task(name, date)
+        self.tasks.append(task)
 
-    def __is_elem_in_list(self, elem):
+    def __is_elem_in_list(self, elem: Task):
         return self.tasks.count(elem) > 0
 
-    def remove(self, elem):
+    def __get_task(self, name: str):
+        for task in self.tasks:
+            if task.name == name:
+                return task
+        return None
+
+    def remove(self, elem: Task):
         if self.__is_elem_in_list(elem):
             self.tasks.remove(elem)
+            print("task was removed")
+        print("task not found, so cannot be removed")
 
+    def complete_task(self, name: str):
+        task = self.__get_task(name)
+        try:
+            task.fulfill_task()
+        except:
+            print("Task not found, so cannot be completed")
 
     def __str__(self):
-        return str(self.tasks)
-
-
-if __name__ == '__main__':
-    test = ToDoList()
-    test.add(2)
-    print(test)
-
+        output = ""
+        for elem in self.tasks:
+            output += str(elem)
+            output += "\n"
+        return output
 
--- main/src/toDoList/task.py
+++ main/src/toDoList/task.py
@@ -1,9 +1,15 @@
+from datetime import datetime
+
 class Task:
-    def __init__(self, name, deadline):
+    def __init__(self, name: str, deadline):
         self.name = name
         self.is_done = False
-        self.deadline = deadline
+        self.deadline = datetime.strptime(deadline, '%d/%m/%y %H:%M')
+        self.points = 5
 
+    def fulfill_task(self):
+        self.is_done = True
+        print(f'Task {self.name} is completed')
 
     def __str__(self):
         return f"Name: {self.name}, is done {self.is_done}, deadline until {self.deadline}"
